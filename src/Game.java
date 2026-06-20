5b76e8cbca22ac8c465503f1796c73cb0aa84d5c
arturooreaf <aoreaf@gmail.com>
2026-06-20 22:28:32 +0200

    feat: add Game class with title and releaseYear

---
+++ src/Game.java
@@ -0,0 +1,13 @@
+public class Game {
+    //region atributos
+    private String title;
+    private int releaseYear;
+
+    //endregion
+//region Constructore
+    public Game(String title, int releaseYear) {
+        this.title = title; // "el atributo de este objeto = el valor que me pasaron
+        this.releaseYear = releaseYear;
+    }
+//endregion
+}
