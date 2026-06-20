b75ceeb8e5261223d4fd8342ed9d5871cb999e3d
arturooreaf <aoreaf@gmail.com>
2026-06-20 22:38:22 +0200

    Add getters and setters

--- src/Game.java
+++ src/Game.java
@@ -3,6 +3,22 @@ public class Game {
     private String title;
     private int releaseYear;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
     //endregion
 //region Constructore
     public Game(String title, int releaseYear) {
