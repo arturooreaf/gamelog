b4fde271257db3345ab90b19a889a85bbcdf010f
arturooreaf <aoreaf@gmail.com>
2026-06-26 11:43:01 +0200

    feat: add CatalogItem abstract class and refactor Game to extend it

--- src/clases/Game.java
+++ src/clases/Game.java
@@ -1,21 +1,18 @@
 package clases;
 
 import enumerados.GameStatus;
-import jdk.jshell.Snippet;
 
 
-public class Game {
+
+public class Game extends CatalogItem {
     //region attributes/fields
-    private String title;
-    private int releaseYear;
+
     private Developer developer;
     private GameStatus status;
 
     //endregion
 //region Getters and Setters
-    public String getTitle() {
-        return title;
-    }
+
 
     public Developer getDeveloper() {
         return developer;
@@ -35,29 +32,14 @@ public class Game {
         this.status = status;
     }
 
-    public void setTitle(String title) {
-        validateTitle(title);
-        this.title = title;
-    }
 
-    public int getReleaseYear() {
-        return releaseYear;
-    }
-
-    public void setReleaseYear(int releaseYear) {
-        validateReleaseYear(releaseYear);
-        this.releaseYear = releaseYear;
-    }
 
     //endregion
 //region Constructor
     public Game(String title, int releaseYear, Developer developer, GameStatus status) {
-        validateTitle(title);
-        validateReleaseYear(releaseYear);
+        super(title, releaseYear);
         validateStatus(status);
         validateDeveloper(developer);
-        this.title = title; // "el atributo de este objeto = el valor que me pasaron
-        this.releaseYear = releaseYear;
         this.developer = developer;
         this.status = status;
     }
@@ -66,22 +48,19 @@ public class Game {
 
     //endregion
     //region Methods
+
     @Override
     public String toString() {
-        return "The game is: " + this.title + " (" + this.releaseYear + ") | " + this.developer;
-    }
-//endregion
-    private void validateReleaseYear(int releaseYear) {
-        if (releaseYear < 1958 || releaseYear >= 2050) {
-            throw new IllegalArgumentException("The game must have been released in a positive year");
-        }
+        return "Game {" +
+                "title ="+ getTitle() +
+                ", releaseYear="+getReleaseYear() +
+                ", developer=" + developer +
+                ", status=" + status +
+                '}';
     }
 
-    private void validateTitle(String title) {
-        if (title == null || title.isBlank()) {
-            throw new IllegalArgumentException("A game must have a title");
-        }
-    }
+    //endregion
+
 private void validateStatus(GameStatus status){
     if (status == null) throw new IllegalArgumentException("status cannot null ");
 }
